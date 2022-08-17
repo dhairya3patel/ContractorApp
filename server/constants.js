ccca8f3911cb51ee52f9a50ccaf34c53929c5522
dhairya3patel <43616736+dhairya3patel@users.noreply.github.com>
2022-08-17 16:14:39 -0400

    Added Frontend Hashing to Password

--- server/constants.js
+++
@@ -1,5 +0,0 @@
-const MONGO_USER = "dhairyaPatel"
-const MONGO_PASSWORD = "4XiWnkxb915Rna4L"
-
-const ATLAS_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.pki6l.mongodb.net/contractor?retryWrites=true&w=majority`
-export default ATLAS_URI;
--- src/constants.js
+++
@@ -1,5 +0,0 @@
-const API_URL = "http://10.0.2.2:5000";
-const SALT = '$2a$12$e2fZBhQemVrdNGxDeA9c7u'
-
-
-module.exports = { API_URL,SALT}
